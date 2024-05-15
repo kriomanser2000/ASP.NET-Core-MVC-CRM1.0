50ce4b59318066d131c5a0d4bd5f0a9eb597f40c
kriomanser2000 <160469777+kriomanser2000@users.noreply.github.com>
2024-05-15 18:49:59 +0300

    Add project files.

---
+++ Customer Relationship Management 1.0/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Customer_Relationship_Management_1._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
