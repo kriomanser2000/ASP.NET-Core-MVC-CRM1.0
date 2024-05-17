f49282dccb6f257a1d724f83c9e6cf33c278dd6b
kriomanser2000 <160469777+kriomanser2000@users.noreply.github.com>
2024-05-17 17:05:18 +0300

    add db and more more more

--- Customer Relationship Management 1.0/Program.cs
+++ Customer Relationship Management 1.0/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
